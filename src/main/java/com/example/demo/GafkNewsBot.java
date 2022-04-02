6eb3005c70a971e54a6215b5de8699cf7f63631d
Kirill Nizhnik <kiril.niznik@gmail.com>
2022-04-03 00:54:38 +0300

    TEST

--- src/main/java/com/example/demo/GafkNewsBot.java
+++ src/main/java/com/example/demo/GafkNewsBot.java
@@ -8,7 +8,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
